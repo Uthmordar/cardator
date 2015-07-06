96c703f2b553e008881388355138a3af586a34a4
Tanguy <tanguyrygodin@gmail.com>
2015-07-06 15:26:51 +0200

    add trait TraitOrganization for place

--- src/Uthmordar/Cardator/Card/lib/Place/CivicStructure/PoliceStation.php
+++ src/Uthmordar/Cardator/Card/lib/Place/CivicStructure/PoliceStation.php
@@ -7,42 +7,9 @@ class PoliceStation extends CivicStructure {
     protected $parents = "Thing\Place\CivicStructure::Thing\Organization\LocalBusiness\EmergencyService";
     protected $currenciesAccepted;
     protected $openingHours;
-    protected $parentOrganization;
-    protected $paymentAccepted;
-    protected $priceRange;
-    protected $address;
-    protected $aggregateRating;
-    protected $award;
-    protected $brand;
-    protected $contactPoint;
-    protected $department;
-    protected $dissolutionDate;
-    protected $duns;
-    protected $email;
-    protected $employee;
-    protected $event;
-    protected $faxNumber;
-    protected $founder;
-    protected $foundingDate;
-    protected $foundingLocation;
-    protected $globalLocationNumber;
-    protected $hasPOS;
-    protected $isicV4;
-    protected $legalName;
-    protected $location;
-    protected $logo;
-    protected $makesOffer;
-    protected $member;
-    protected $memberOf;
-    protected $naics;
-    protected $numberOfEmployees;
-    protected $owns;
-    protected $review;
-    protected $seeks;
-    protected $subOrganization;
-    protected $taxID;
-    protected $telephone;
-    protected $vatID;
+
+    use TraitOrganization;
+
     protected $type = "http://schema.org/PoliceStation";
 
 }
---
+++ src/Uthmordar/Cardator/Card/lib/Place/CivicStructure/TraitOrganization.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Uthmordar\Cardator\Card\lib;
+
+trait TraitOrganization {
+
+    protected $parentOrganization;
+    protected $paymentAccepted;
+    protected $priceRange;
+    protected $award;
+    protected $brand;
+    protected $contactPoint;
+    protected $department;
+    protected $dissolutionDate;
+    protected $duns;
+    protected $email;
+    protected $employee;
+    protected $founder;
+    protected $foundingDate;
+    protected $foundingLocation;
+    protected $hasPOS;
+    protected $legalName;
+    protected $location;
+    protected $makesOffer;
+    protected $member;
+    protected $memberOf;
+    protected $naics;
+    protected $numberOfEmployees;
+    protected $owns;
+    protected $seeks;
+    protected $subOrganization;
+    protected $taxID;
+    protected $vatID;
+
+}
